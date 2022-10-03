c6f9cf4710ee3a8467295cd81096b47bf9b82b4e
Kishor Krishna <kishorkrishna3k@gmail.com>
2022-10-03 20:03:57 +0530

    fixed lot of bugs,and improved logic,reduced code

--- game.js
+++ game.js
@@ -3,6 +3,32 @@ const resetGame = () => {
 };
 let currentTurnBtnClassName = "btn-X";
 
+const checkDraw = () => {
+  return Array.from(document.querySelectorAll(".cell")).every((cell) => {
+    return cell.classList.contains("btn-X") || cell.classList.contains("btn-O");
+  });
+};
+
+const extractState = (cellClassName, index) => {
+  console.log(cellClassName);
+  return Array.from(document.querySelector(cellClassName).classList)
+    .join("")
+    .replace("cellcell-" + index, "");
+};
+
+const compareCells = (firstState, secondState, thirdState) => {
+  if (
+    firstState === secondState &&
+    firstState === thirdState &&
+    firstState != ""
+  ) {
+    document.querySelector(".game-title").innerText =
+      "Player " +
+      firstState.substring(firstState.indexOf("-") + 1, firstState.length) +
+      " Wins!";
+  }
+};
+
 const checkForWin = () => {
   const cellOneState = extractState(".cell-1", 1);
   const cellTwoState = extractState(".cell-2", 2);
@@ -28,28 +54,7 @@ const checkForWin = () => {
   }
 };
 
-const checkDraw = () => {
-  return Array.from(document.querySelectorAll(".cell")).every((cell) => {
-    return cell.classList.contains("btn-X") || cell.classList.contains("btn-O");
-  });
-};
-const compareCells = (firstState, secondState, thirdState) => {
-  if (
-    firstState === secondState &&
-    firstState === thirdState &&
-    firstState != ""
-  ) {
-    document.querySelector(".game-title").innerText =
-      "Player " +
-      firstState.substring(firstState.indexOf("-") + 1, firstState.length) +
-      " Wins!";
-  }
-};
-
 const shakeCell = (cell) => {
-
-if (cell.nodeName !== "DIV") 
-cell= cell.parentElement;
   cell.classList.add("shaking");
 
   setTimeout(() => {
@@ -57,17 +62,10 @@ cell= cell.parentElement;
   }, 400);
 };
 
-const extractState = (cellClassName, index) => {
-  return Array.from(document.querySelector(cellClassName).classList)
-    .join("")
-    .replace("cellcell-" + index, "");
-};
-
-const updateCellClassList = (e) => {
-
-    e.target.classList.add(currentTurnBtnClassName);
-    if (currentTurnBtnClassName == "btn-X") {
-      e.target.innerHTML = `
+const renderSvg = (target) => {
+  target.classList.add(currentTurnBtnClassName);
+  if (currentTurnBtnClassName == "btn-X") {
+    target.innerHTML = `
         <svg
         
                 class="btn-X-icon"
@@ -88,8 +86,8 @@ const updateCellClassList = (e) => {
                 {" "}
               </svg>
        `;
-    } else {
-      e.target.innerHTML = `
+  } else {
+    target.innerHTML = `
       <svg
       x="0px"
       y="0px"
@@ -105,10 +103,6 @@ const updateCellClassList = (e) => {
       <path d="M13.917 0C6.244 0 .001 6.243.001 13.918c0 7.673 6.243 13.916 13.916 13.916s13.916-6.243 13.916-13.916C27.833 6.243 21.59 0 13.917 0zm0 24.834c-6.021 0-10.916-4.896-10.916-10.916C3.001 7.897 7.897 3 13.917 3c6.02 0 10.916 4.896 10.916 10.918 0 6.02-4.896 10.916-10.916 10.916z"></path>
     </svg>
        `;
-    }
-  } else {
-    shakeCell(e.target);
-    
   }
 };
 
@@ -117,24 +111,36 @@ const toggleCurrentTurnBtnClassName = () => {
     currentTurnBtnClassName == "btn-X" ? "btn-O" : "btn-X";
 };
 window.addEventListener("load", () => {
-  console.log("game loaded");
   document.querySelector(".btn-restart").addEventListener("click", resetGame);
   document.querySelectorAll(".cell").forEach((cell) => {
-    cell.addEventListener("click", (e) => {
-if (
-    !(
-      e.target.classList.contains(".btn-X") ||
-      e.target.classList.contains(".btn-O")
-    ) &&(
-    e.target.nodeName === "DIV"&&
-document.querySelector(".game-title").innerText === "Tic Tac Toe"
-  ))
-      {
-        updateCellClassList(e);
+    cell.addEventListener("click", ({ target }) => {
+      switch (target.nodeName) {
+        case "DIV":
+          break;
+        case "svg":
+          target = target.parentElement;
+          break;
+        case "line":
+          target = target.parentElement.parentElement;
+          break;
+        case "path":
+          target = target.parentElement.parentElement;
+          break;
+        default:
+      }
+      if (
+        document.querySelector(".game-title").innerText === "Tic Tac Toe" &&
+        !(
+          target.classList.contains("btn-X") ||
+          target.classList.contains("btn-O")
+        )
+      ) {
+        renderSvg(target);
         checkForWin();
+
         toggleCurrentTurnBtnClassName();
       } else {
-        shakeCell(e.target);
+        shakeCell(target);
       }
     });
   });
